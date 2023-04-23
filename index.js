6b1ea0cc582c1f82fb8859fcbfe30431b0f983fb
Abhilasha Ganvir <nilesh_rmtk@yahoo.com>
2023-04-23 19:20:09 +0530

    Added logic for signup route

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const { use } = require('express/lib/application');
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
+let users = [];
 //Sending text response
 app.get('/', (req,res) => {
     res.send('Hello World!');
@@ -19,15 +24,51 @@ app.get('/route2', (req,res) => {
 });
 
 
-app.get('/signUp', (req,res) => {
+app.post('/signup', (req,res) => {
+    //Add logic to decode the body
+    //Body should have email and password
+    
+    let {email, password, role} = req.body;
+    // Add user to the users array
+   
+    if(email && password && role){
+        if(users != null && users != undefined){
+            if(users.length > 0){
+                let user = users.find(user => user.email === email);
+                if(user){
+                    res.send(`User already exists with ${email}`).status(200);
+                    return
+                }
+            }
+            users.push(req.body);
+            res.send(`Your signup with ${email} is successful.`).status(200);
+        }
+        
+    }
+    else{
+        res.status(401);
+        res.send(`Your signup with ${email} failed..`);
+    }
+    console.log("Users " + JSON.stringify(users));
+});
+
+app.post('/login', (req,res) => {
+    //Add logic to decode the body
+    //Body should have email and password
+
+    //
     res.send(`<html><h1 style="color:green;">Hello World</h1></html>`);
 });
 
-app.get('/login', (req,res) => {
+app.get('/questions', (req,res) => {
+    res.send(`<html><h1 style="color:green;">Hello World</h1></html>`);
+});
+
+app.post('/submissions', (req,res) => {
     res.send(`<html><h1 style="color:green;">Hello World</h1></html>`);
 });
 
 
 app.listen(port , () => {
-    console.log(`App is listering on ${port}`);
+    console.log(`App is listening on ${port}`);
 });
